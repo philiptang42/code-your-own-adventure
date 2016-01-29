bda0d2f2fb54895164dc88b05a84c9bd8044b30a
philiptang42 <philiptang42@gmail.com>
2016-01-28 22:05:27 -0500

    rating bait

--- code_your_own_adventure.js
+++ code_your_own_adventure.js
@@ -9,3 +9,19 @@ else {
 console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'")
 console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'")
 var userAnswer = prompt("Do you want to race Beiber on stage?")
+if(userAnswer = "yes")
+{
+    console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
+}
+else {
+    console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
+}
+var feedback = prompt("rate me")
+if(feedback > 8)
+{
+    console.log("Thank you! We should race at the next concert!");
+}
+    else
+    {
+        ("I'll keep practicing coding and racing.");
+    }
