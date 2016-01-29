790febb4d889cff673c39345b1f4f659f85d6096
philiptang42 <philiptang42@gmail.com>
2016-01-28 21:58:08 -0500

    first move

--- code_your_own_adventure.js
+++ code_your_own_adventure.js
@@ -7,3 +7,5 @@ else {
     console.log("let's do it");
     }
 console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'")
+console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'")
+var userAnswer = prompt("Do you want to race Beiber on stage?")
